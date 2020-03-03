9f016c96b3173ee32360a3184f45f2c6cfe20797
Zachary Heil <zheilb@me.com>
2020-03-03 17:07:37 -0600

    Copied mapEmployeeData to Employee branch for test

--- src/controllers/commands/employees/helpers/employeeHelper.ts
+++ src/controllers/commands/employees/helpers/employeeHelper.ts
@@ -1,4 +1,6 @@
 import { EmployeeClassification } from '../../models/constants/entityTypes';
+import { Employee } from '../../../typeDefinitions';
+import { EmployeeModel } from '../../models/employeeModel';
 
 export const hashString = (toHash: string): string => {
 	return ''; // TODO: Look at https://nodejs.org/docs/latest-v12.x/api/crypto.html#crypto_crypto_createhash_algorithm_options as one option
@@ -6,4 +8,18 @@ export const hashString = (toHash: string): string => {
 
 export const isElevatedUser = (employeeClassification: EmployeeClassification): boolean => {
 	return false; // TODO: Determine if an employee is an elevated user by their classification
+};
+
+export const mapEmployeeData = (queriedEmployee: EmployeeModel): Employee => {
+	return <Employee>{
+		id: queriedEmployee.id,
+		active: queriedEmployee.active,
+		lastName: queriedEmployee.lastName,
+		password: queriedEmployee.password,
+		createdOn: queriedEmployee.createdOn,
+		firstName: queriedEmployee.firstName,
+		managerId: queriedEmployee.managerId,
+		employeeId: queriedEmployee.employeeId.toString(),
+		classification: queriedEmployee.classification
+	};
 };
