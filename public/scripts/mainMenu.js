e97de8d1b1ce68d49494ba38745a5c2352ce32b0
unknown <bakerwill764@gmail.com>
2020-03-03 21:50:28 -0600

    fixed button not working for non-elevated user

--- public/scripts/mainMenu.js
+++ public/scripts/mainMenu.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const transactionButton = document.getElementById("startTransactionButton");
-	const cashierReportButton = document.getElementById("cashierReportButton");
 	const productListButton = document.getElementById("productListButton");
+
+	const cashierReportButton = document.getElementById("cashierReportButton");
 	const employeeDetailsButton = document.getElementById("employeeDetailsButton");
 	const salesReportButton = document.getElementById("salesReportButton");
 
 	transactionButton.addEventListener("click", missingFunctionalityError);
+	productListButton.addEventListener("click", function(){ console.log('Product Button Clicked.'); location.assign("http://localhost:15100/productListing"); });
+
 	cashierReportButton.addEventListener("click", missingFunctionalityError);
 	salesReportButton.addEventListener("click", missingFunctionalityError);
-
-	productListButton.addEventListener("click", function(){ location.assign("http://localhost:15100/productListing"); });
 	employeeDetailsButton.addEventListener("click", function(){ location.assign("http://localhost:15100/employeeDetail") });
 });
 
