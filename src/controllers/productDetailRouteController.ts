a122722a1b6c41d426b89bf0c54dbe3fa6ec5f21
Zachary Heil <zheilb@me.com>
2020-03-04 20:12:29 -0600

    Fixed display of Delete and Save Buttons

--- src/controllers/productDetailRouteController.ts
+++ src/controllers/productDetailRouteController.ts
@@ -28,12 +28,18 @@ const processStartProductDetailError = (res: Response, error: any): void => {
 };
 
 export const start = async (req: Request, res: Response): Promise<void> => {
-	return ProductQuery.queryById(req.params[ParameterLookup.ProductId])
-		.then((productsCommandResponse: CommandResponse<Product>): void => {
+	let elevated = false;
+	return ValidateActiveUser.execute((<Express.Session>req.session).id)
+		.then((activeUserCommandResponse: CommandResponse<ActiveUser>): void => {
+			elevated = activeUserCommandResponse.data?.classification! > 101 ? true : false;
+		}).then((): Promise<CommandResponse<Product>> => {
+			return ProductQuery.queryById(req.params[ParameterLookup.ProductId]);
+		}).then((productsCommandResponse: CommandResponse<Product>): void => {
 			return res.render(
 				ViewNameLookup.ProductDetail,
 				<ProductDetailPageResponse>{
-					product: productsCommandResponse.data
+					product: productsCommandResponse.data,
+					isElevatedUser: elevated 
 				});
 		}).catch((error: any): void => {
 			return processStartProductDetailError(res, error);
